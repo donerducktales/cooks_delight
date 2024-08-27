05ee5063c533b45910f5113c317c49ba8bf899cd
donerducktales <randomercook231@gmail.com>
2024-08-27 13:24:22 +0300

    додані майже всі шрифти, та поставлено шрифт roboto в мейні як основний, додано папку components

---
+++ app/assets/fonts.ts
@@ -0,0 +1,11 @@
+import { Montserrat, Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+   subsets: ['latin'],
+   weight: ['300', '400', '500', '700',],
+});
+
+export const montserrat = Montserrat({
+   subsets: ['latin'],
+   weight: ['600', '700']
+})
