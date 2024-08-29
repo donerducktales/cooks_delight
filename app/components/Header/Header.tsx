a592ee2666a31bd1a9686437796685e0788782cb
donerducktales <randomercook231@gmail.com>
2024-08-29 23:24:33 +0300

    зроблено хедер поки без кнопок

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import '../Header/header.scss'
+
+export default function Header() {
+   return (
+      <header className="header">
+         <div className="header_container">
+            ghjkjhgfd
+         </div>
+      </header>
+   )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
 import { roboto } from "./assets/fonts";
+import Header from "./components/Header/Header";
 
 export const metadata: Metadata = {
    title: "Home",
@@ -15,6 +16,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={roboto.className}>
+            <Header />
             {children}
          </body>
       </html>
