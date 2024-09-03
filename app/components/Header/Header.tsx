ac02fa4a68840f121de7d47581f687bcec28f354
donerducktales <randomercook231@gmail.com>
2024-09-03 17:53:01 +0300

    зроблено хедер по макету, але поки без адаптиву і підсвітки навігаційних посилань

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,35 @@
+import Image from 'next/image'
 import '../Header/header.scss'
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
 
 export default function Header() {
    return (
       <header className="header">
          <div className="header_container">
-            ghjkjhgfd
+            <nav className="nav">
+               <div className="nav_logo">
+                  <Image 
+                     width={180}
+                     height={35}
+                     alt='nav_logo'
+                     src='/nav-logo.svg'
+                  />
+               </div>
+               <ul className='nav_links'>
+                  <li>Home</li>
+                  <li>Recipes</li>
+                  <li>Cooking tips</li>
+                  <li>About Us</li>
+               </ul>
+               <div className='nav_buttons'>
+                  <button className="search-button">
+                     <MagnifyingGlassIcon />
+                  </button>
+                  <button className="subscribe-button">
+                     Subscribe
+                  </button>
+               </div>
+            </nav>
          </div>
       </header>
    )
