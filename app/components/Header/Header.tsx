98cc1d3ee36c1dc2d852dbc6d1c2cb430b9fb193
donerducktales <randomercook231@gmail.com>
2024-09-21 23:38:50 +0300

    трохи перероблено структуру хедеру, точніше меню гамбургера, тепер всі хуки беспосередньо в хедері, переписані класнейми, також при натисканні на гамбургер, то хедер міняє колір на темний(але треба доробити, бо текст лого має бути білим, також вся сторінка при натисканні має бути темною згідно макету)

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,14 +1,26 @@
-import Image from 'next/image'
-import '../Header/header.scss'
-import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
-import MenuMobile from './MenuMobile/MenuHamburgerButton'
+'use client'
+
+import Image from 'next/image';
+import styles from '@/app/components/Header/header.module.scss';
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid';
+import { useState } from 'react';
+import variables from '@/app/styles/_variables.module.scss';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 
 export default function Header() {
+   const [open, setOpen] = useState<boolean>(false);
+
+   function toDark(colorProperty: string) {
+      if (open) {
+         return { backgroundColor: colorProperty, }
+      }
+   }
+
    return (
-      <header className="header">
-         <div className="header_container">
-            <nav className="nav">
-               <div className="nav_logo">
+      <header className={styles.header} style={toDark('rgb(38, 37, 34, 0.9)')}>
+         <div className={styles.header_container} style={toDark(variables.dark)}>
+            <nav className={styles.nav}>
+               <div className={styles.nav_logo}>
                   <Image 
                      width={180}
                      height={35}
@@ -17,21 +29,27 @@ export default function Header() {
                      priority
                   />
                </div>
-               <ul className='nav_links'>
+               <ul className={styles.nav_links}>
                   <li>Home</li>
                   <li>Recipes</li>
                   <li>Cooking tips</li>
                   <li>About Us</li>
                </ul>
-               <div className='nav_buttons'>
-                  <button className="search-button">
+               <div className={styles.nav_buttons}>
+                  <button className={styles.searchButton}>
                      <MagnifyingGlassIcon />
                   </button>
-                  <button className="subscribe-button">
+                  <button className={styles.subscribeButton}>
                      Subscribe
                   </button>
                </div>
-               <MenuMobile />
+               <button 
+                  className={styles.menuHamburgerButton}
+                  onClick={() => setOpen(!open)}
+                  style={toDark('rgba(255, 251, 242, 0.26)')}
+               >
+                  {open ? <XMarkIcon color={variables.primaryOrange} /> : <Bars3Icon />}
+               </button>
             </nav>
          </div>
       </header>
