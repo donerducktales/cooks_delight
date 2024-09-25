893d6ed5df30e77e6cba4214ed551842dd9fd6bb
donerducktales <randomercook231@gmail.com>
2024-09-25 22:06:14 +0300

    змінив логотип так, щоб можна було постивити анімацію зі зміною кольору тексту лого, реалізовано transition для хедеру

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -4,15 +4,27 @@ import Image from 'next/image';
 import styles from '@/app/components/Header/header.module.scss';
 import { MagnifyingGlassIcon } from '@heroicons/react/16/solid';
 import { useState } from 'react';
-import variables from '@/app/styles/_variables.module.scss';
+import scssvariables from '@/app/styles/_variables.module.scss';
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import MenuMobile from './MenuMobile/MenuMobile';
 import styled from 'styled-components';
 
-const HeaderContainer: any = styled.div<{ open?: boolean }>`
+// const HeaderStyled = styled.header<{open?: boolean}>`
+
+// `;
+
+const HeaderContainer = styled.div<{ open?: boolean }>`
    @media (max-width: 800px) {
-      background-color: ${({ open }) => open && variables.dark};
+      background-color: ${({ open }) => open && `rgba(255, 251, 242, 0.05)`};
       border-radius: ${({ open }) => open && '24px 24px 0 0'};
+      transition: background-color 0.7s;
+   }
+`;
+
+const NavLogoText = styled.div<{ open?: boolean }>`
+   @media (max-width: 800px) {
+      color: ${({ open }) => open && scssvariables.background};
+      transition: color 0.7s;
    }
 `;
 
@@ -21,12 +33,15 @@ export default function Header() {
 
    function toDark(colorProperty: string) {
       if (open) {
-         return { backgroundColor: colorProperty, }
+         return { backgroundColor: colorProperty, transition: 'background-color 0.7s'}
       }
    }
 
    return (
-      <header className={styles.header} style={toDark('rgb(38, 37, 34, 0.9)')}>
+      <header
+         className={styles.header} 
+         style={toDark(scssvariables.dark)}
+      >
          <HeaderContainer 
             open={open}
             className={styles.headerContainer} 
@@ -34,12 +49,19 @@ export default function Header() {
             <nav className={styles.nav}>
                <div className={styles.navLogo}>
                   <Image 
-                     width={180}
+                     width={40}
                      height={35}
                      alt='nav_logo'
                      src='/nav-logo.svg'
                      priority
+                     quality={100}
                   />
+                  <NavLogoText
+                     open={open} 
+                     className={styles.navLogoText}
+                  >
+                     <p>Cooks Delight</p>
+                  </NavLogoText>
                </div>
                <ul className={styles.navLinks}>
                   <li>Home</li>
@@ -60,11 +82,11 @@ export default function Header() {
                   onClick={() => setOpen(!open)}
                   style={toDark('rgba(255, 251, 242, 0.26)')}
                >
-                  {open ? <XMarkIcon color={variables.primaryOrange} /> : <Bars3Icon />}
+                  {open ? <XMarkIcon color={scssvariables.primaryOrange} /> : <Bars3Icon />}
                </button>
             </nav>
-            {open && <MenuMobile />}
          </HeaderContainer>
+         {open && <MenuMobile open={open} />}
       </header>
    )
 }
--- app/components/Header/MenuMobile/MenuMobile.tsx
+++ app/components/Header/MenuMobile/MenuMobile.tsx
@@ -1,9 +1,28 @@
-import styles from '@/app/components/Header/MenuMobile/menu-mobile.module.scss'
+import styles from '@/app/components/Header/MenuMobile/menu-mobile.module.scss';
+import styled from 'styled-components';
+import scssvariables from '@/app/styles/_variables.module.scss';
 
-export default function MenuMobile() {
+const StyledMenuMobile = styled.div<{open?: boolean}>`
+   @media (max-width: 800px) {
+      background-color: ${scssvariables.dark};
+      transition: background-color 3s;
+
+      nav {
+         width: 100%;
+         margin: 0 16px;
+         padding: 0 16px;
+         background-color: rgba(255, 251, 242, 0.05);
+         border-radius: 0 0 24px 24px;
+      }
+   }
+`;
+
+export default function MenuMobile({open}: {open: boolean}) {
    return (
-      <div className={styles.menuMobile}>
-         Menumobile
-      </div>
+      <StyledMenuMobile className={styles.menuMobile}>
+         <nav>
+            f
+         </nav>
+      </StyledMenuMobile>
    )
 }
