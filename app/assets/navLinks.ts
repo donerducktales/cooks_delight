41b0c20c7e5a64c2ebd5d04686b2adca29a55f10
Koval Sviatoslav <randomercook231@gmail.com>
2025-03-11 16:03:13 +0200

    повернув старе посилання в camelcase

--- app/assets/navLinks.ts
+++ app/assets/navLinks.ts
@@ -8,5 +8,5 @@ export const links: links[] = [
    { id: 0, name: 'Home', path: '/' },
 	{ id: 1, name: 'Recipes', path: '' },
 	{ id: 2, name: 'Cooking tips', path: '/tips' },
-   { id: 3, name: 'About Us', path: '/aboutus' },
+   { id: 3, name: 'About Us', path: '/AboutUs' },
 ];
