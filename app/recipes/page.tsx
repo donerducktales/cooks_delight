cec64b58ae9a359f511442bbd61dff060b3551c3
Koval Sviatoslav <randomercook231@gmail.com>
2025-07-25 23:28:37 +0300

    виправив футер(там у нав кнопок я забув лишній бордер забрати), додав metadata для page.tsx recipes, переписав CTAsubscribeSection піл tailwind і styled-components

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,13 +1,22 @@
+import { Metadata } from "next";
 import FetchRecipesSection from "../components/RecipesSection/FetchRecipesSection";
 import TipsAnnotationSection from "../layouts/TipsAnnotationSection/TipsAnnotationSection";
 
+export const metadata: Metadata = {
+  title: "Recipes",
+};
+
 export default function RecipesPage() {
-   return (
-      <main className={`flex flex-col justify-between items-center min-h-screen w-full ${'main'}`}>
-         <div className={`flex flex-col items-center w-[calc(100%-2rem)] max-w-[1312px]  ${'mainContainer'}`}>
-            <FetchRecipesSection />
-            <TipsAnnotationSection />
-         </div>
-      </main>
-   )
+  return (
+    <main
+      className={`flex flex-col justify-between items-center min-h-screen w-full ${"main"}`}
+    >
+      <div
+        className={`flex flex-col items-center w-[calc(100%-2rem)] max-w-[1312px]  ${"mainContainer"}`}
+      >
+        <FetchRecipesSection />
+        <TipsAnnotationSection />
+      </div>
+    </main>
+  );
 }
